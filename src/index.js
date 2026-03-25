b3689bf24e1940c758b8f3701ed0584389664a99
antjekunert4-png <antjekunert4@gmail.com>
2026-03-25 18:51:00 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Give me a sign. I remember such a blue sky that gave me a sign.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
